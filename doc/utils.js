191c0be89cb35cf9982481bc1ac3f64d0663a554
EA10-lite <71076800+EA10-lite@users.noreply.github.com>
2021-06-07 07:26:19 -0700

    Delete doc directory

--- doc/utils.js
+++
@@ -1,2 +0,0 @@
-const img = ["images/icon-scissors.svg","images/icon-paper.svg","images/icon-rock.svg","images/icon-lizard.svg","images/icon-spock.svg"];
-const players = ["scissors","paper","rock","lizard","spock"]
